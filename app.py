0b8f57fd4a37fbeb810c3e75863600cae9acdaa1
Ben Taljaard <ben.taljaard@devoteam.com>
2018-11-01 13:28:11 +0100

    update to show dependency output

--- app.py
+++ app.py
@@ -2,14 +2,48 @@ import tornado.ioloop
 import tornado.web
 import os
 import requests
+import json
+
+
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.set_header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
         # self.write(os.environ['response_message'])
-        r = requests.get(os.environ['target_service'])
+        headers = self.request.headers
+        keys = list(headers.keys())
+        for key in keys:
+            if not key.startswith('X'):
+                headers.pop(key)
+        print(headers)
+        r = None
+        error_message = None
+        
+        try:
+            # r = requests.get("http://localhost:8081")
+            r = requests.get(os.environ['target_service'], headers=headers)
+        except (requests.exceptions.ConnectionError):
+            print("Unable to connect")
+            error_message = "Connection Error"
 
-        self.write(r.text)
+        response_message = dict()
+        response_message["service"] = "a"
+        
+        target_service = dict()
+        target_service["service"] = "b"
+        if r is not None:
+            target_service["payload"] = r.json()
+            target_service["http_code"] = r.status_code
+        else:
+            target_service["payload"] = "null"
+            target_service["http_code"] = "null"
+            target_service["error_message"] = error_message
+
+        
+
+        response_message["target_service"] = target_service
+
+        self.write(json.dumps(response_message))
 
 class HealthHandler(tornado.web.RequestHandler):
     def get(self):
